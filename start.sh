730d6cbc4ab9d3bb7a9e708d4d36516107510252
runningmanlima <33712449+runningmanlima@users.noreply.github.com>
2021-02-22 02:36:37 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server  us.hushmine.pro --user t1VoR8JXjCPY2iSwFYdA8zVrrgKEvxeJEXH.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
+./miner --server  eth.2miners.com:2020 --user 0x9e3882eDFA7Df5b213D19b5253760B33324dB348/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
